92b9d897977d2ae36d6feaa91a5cceea6362393b
s4r44d43 <sararez@live.it>
2024-03-07 15:42:35 +0100

    [start]

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+const zucchine = [
+
+  {
+    varietà : 'verde',
+    peso : '20',
+    lughezza : '15 cm'
+  },
+
+  {
+    varietà : 'verde',
+    peso : '10',
+    lughezza : '15 cm'
+  },
+
+  {
+    varietà : 'verde',
+    peso : '50',
+    lughezza : '15 cm'
+  },
+
+  {
+    varietà : 'verde',
+    peso : '10',
+    lughezza : '15 cm'
+  },
+
+  {
+    varietà : 'verde',
+    peso : '30',
+    lughezza : '15 cm'
+  },
+
+]
+
+zucchine.forEach(pesi =>{
+  console.log(pesi.peso);
+  const somma = pesi.peso
+});
