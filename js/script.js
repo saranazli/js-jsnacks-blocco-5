245c913f73aa16699d139b519616e1c0eb60c9ea
s4r44d43 <sararez@live.it>
2024-03-08 12:31:47 +0100

    [correction]

--- js/script.js
+++ js/script.js
@@ -81,39 +81,64 @@ console.log(myStringRev);
 
 // snack 4 fonduta
 
+/*const arrayUno = [ 'ciao', 'sara', 'bene'];
+const arrayDue = [ 'cara', 'stai', '?'];
+let arrayTre = [];*/
+
+// pushWords();
+// pushWords1();
+
+// setInterval(pushWords,2000);
+// setInterval(pushWords1,1000);
+
+
+// function pushWords (){
+//   for (let word of arrayUno){
+    
+//     arrayTre += word;
+
+//   }
+// }
+
+// function pushWords1 (){
+//   for (let word1 of arrayDue){
+    
+//     arrayTre += word1;
+
+//   }
+// } 
+
+// MY WRONG WAY
+
+// the right way
+
 const arrayUno = [ 'ciao', 'sara', 'bene'];
 const arrayDue = [ 'cara', 'stai', '?'];
 let arrayTre = [];
 
-pushWords();
-pushWords1();
+const maggioreLunghezza = arrayUno.length > arrayDue.length ? arrayUno.length : arrayDue.length;
 
-setInterval(pushWords,2000);
-setInterval(pushWords1,1000);
+for(let i = 0; i < maggioreLunghezza; i++) {
 
+  if(arrayUno[i])arrayTre.push(arrayUno[i]);
 
-function pushWords (){
-  for (let word of arrayUno){
-    
-    arrayTre += word;
+  if(arrayDue[i])arrayTre.push(arrayDue[i]);
 
-  }
-}
-
-function pushWords1 (){
-  for (let word1 of arrayDue){
-    
-    arrayTre += word1;
-
-  }
-}
-
-
-
-
+};
 
 console.log(arrayTre);
-console.log();
+
+// Snack 5
+
+const numbers = [ 121, 3, 64, 6, 16, 18, 26];
+const letters = [ 'a', 'b', 'c', 'd']
+
+const min = Math.floor(Math.random() * 5);
+const max = Math.floor(Math.random() * numbers.lenght - min + 1) +min;
+
+const range = ( ) => numbers.filter((el, i) => i >= min && i <= max);
+
+console.log(range(min, max, letters));
 
 
 
